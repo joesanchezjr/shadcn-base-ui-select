5b43255d14ee55cd14e5c778ce5b38d5ecd23b22
Joe Sanchez <joe@joesanchezjr.com>
2025-12-13 11:45:39 -0600

    example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const options = [
+  { value: "a", label: "Option A" },
+  { value: "b", label: "Option B" },
+  { value: "c", label: "Option C" },
+  { value: "d", label: "Option D" },
+];
+
 export function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="font-medium">Hello World</div>
+    <div className="max-w-4xl mx-auto p-48">
+      <Select items={options}>
+        <SelectTrigger className="w-full">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          {options.map((option) => (
+            <SelectItem key={option.value} value={option.value}>
+              {option.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>{" "}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
